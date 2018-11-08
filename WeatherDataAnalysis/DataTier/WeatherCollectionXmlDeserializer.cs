9a7ab753888fb115f498f9d6ebed0d3c2f49891a
jlooney2 <jlooney2@my.westga.edu>
2018-11-07 22:48:11 -0500

    Added code to Serialize and Deserialize the weatherdata collection

---
+++ WeatherDataAnalysis/DataTier/WeatherCollectionXmlDeserializer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+using Windows.Storage;
+using WeatherDataAnalysis.Model;
+
+namespace WeatherDataAnalysis.DataTier
+{
+   public class WeatherCollectionXmlDeserializer
+    {
+        public static WeatherDataCollection XmlToWeatherCollection(StorageFile file)
+        {
+            if (file == null)
+            {
+                throw new ArgumentNullException(nameof(file), "Cannot accept null for a file");
+            }
+            var reader = new XmlSerializer(typeof(WeatherDataCollection));
+            var fileReader = new StreamReader(file.Path);
+            var weatherDataCollection = (WeatherDataCollection)reader.Deserialize(fileReader);
+            fileReader.Close();
+
+            return weatherDataCollection;
+        }
+    }
+}
